677186d980b673f3381cf4e2ed98b5707c7fe39a
Gavin Noronha <gavinln@hotmail.com>
2021-10-20 22:40:21 -0700

    add restaurant example

---
+++ projects/restaurant/src/lib.rs
@@ -0,0 +1,62 @@
+use rand::Rng;
+use crate::front_of_house::hosting;
+mod front_of_house;
+
+// this works too
+// use self::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // doesn't compile, private field
+    // meal.seasonal_fruit = String::from("blueberries");
+    
+    let _order1 = back_of_house::Appetizer::Soup;
+    let _order2 = back_of_house::Appetizer::Salad;
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("{}", secret_number);
+}
+
+fn serve_order() {}
+
+#[allow(dead_code)]
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+
