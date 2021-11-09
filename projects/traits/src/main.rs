17757714bc6d4c6afdcb41f80c13b053040201ac
Gavin Noronha <gavinln@hotmail.com>
2021-11-08 21:11:21 -0800

    add traits example

---
+++ projects/traits/src/main.rs
@@ -0,0 +1,141 @@
+use std::fmt::Display;
+use std::clone::Clone;
+use std::fmt::Debug;
+
+fn main() {
+    println!("Hello, world!");
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from (
+            "of course, as you probably already know, people"
+        ),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL,",
+        ),
+    };
+    println!("New article available! {}", article.summarize());
+
+    #[derive(Debug)]
+    struct Pair<T> {
+        x: T,
+        y: T,
+    }
+
+    impl<T> Pair<T> {
+        fn new(x: T, y: T) -> Self {
+            Self {x, y}
+        }
+    }
+
+    impl<T: Display + PartialOrd> Pair<T> {
+        fn cmp_display(&self) {
+            if self.x > self.y {
+                println!("The largest is x = {}", self.x); 
+            } else {
+                println!("The largest is y = {}", self.y); 
+            }
+        }
+    }
+
+    let p1 = Pair { x: 3, y: 4 };
+    let p2 = Pair::new(3, 5);
+    println!("Pair {:?}", p1);
+    println!("Pair {:?}", p2);
+    p2.cmp_display();
+}
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {}, ({})", self.headline, self.author, self.location)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    // fn summarize(&self) -> String {
+    //     format!("{}: {}", self.username, self.content)
+    // }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn notify_bound<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// pub fn notify2(item1: &impl Summary, item2: &impl Summary)
+// pub fn notify_bound2<T: Summary>(item1: &T, item2: &T)
+
+
+pub fn notify3(item: &(impl Summary + Display)) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn notify_bound3<T: Summary + Display>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn some_function<T: Display + Clone, U: Clone + Debug>(_t: &T, _u: &U) -> i32 {
+    3
+}
+
+pub fn some_function2<T: Display + Clone, U: Clone + Debug>(_t: &T, _u: &U) -> i32 
+    where T: Display + Clone,
+          U: Clone + Debug {
+    3
+}
+
+pub fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    }
+}
+
